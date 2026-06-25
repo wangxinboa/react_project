39e258ff1464ba4ba8e4fa0cf3033d594a98b363
wangxinboa <a634318935@126.com>
2026-06-25 14:36:30 +0800

    从路由器配置及相关导出中移除 CodeFunctionStack 页面

--- src/router/router.js
+++ src/router/router.js
@@ -2,7 +2,6 @@ import { createHashHistory } from "history";
 
 import AnalysisList from "../pages/js_code_analysis/analysis_list/analysis_list.jsx";
 import JsCodeAnalysisResult from "../pages/js_code_analysis/js_code_analysis_result/js_code_analysis_result.jsx";
-import CodeFunctionStack from "../pages/code_function_stack/code_function_stack.jsx";
 import { ProjectList } from "../pages/project_manager/project_list.jsx";
 import { RequirementList } from "../pages/project_manager/requirement_list.jsx";
 import { AIPrompt } from "../pages/ai_prompt/ai_prompt.jsx";
@@ -33,11 +32,6 @@ export const AnalysisPage = {
 	label: "代码分析",
 	children: [AnalysisListPage, JsCodeAnalysisResultPage],
 };
-export const CodeFunctionStackPage = {
-	path: "/CodeFunctionStack",
-	label: "代码函数栈",
-	Component: CodeFunctionStack,
-};
 export const ProjectListPage = {
 	path: "/ProjectManager/ProjectList",
 	label: "项目列表",
@@ -53,14 +47,7 @@ export function toAnalysisContentPage(name, url) {
 	history.push(`${JsCodeAnalysisResultPage.path}?analysis_name=${name}&analysis_url=${url}`);
 }
 
-export const PageItems = [
-	IndexPage,
-	AIPromptPage,
-	AnalysisPage,
-	CodeFunctionStackPage,
-	ProjectListPage,
-	RequirementListPage,
-];
+export const PageItems = [IndexPage, AIPromptPage, AnalysisPage, ProjectListPage, RequirementListPage];
 
 export const MenuItems = [
 	{
@@ -70,10 +57,6 @@ export const MenuItems = [
 	{
 		key: AnalysisListPage.path,
 		label: AnalysisListPage.label,
-	},
-	{
-		key: CodeFunctionStackPage.path,
-		label: CodeFunctionStackPage.label,
 	},
 	{
 		key: "project-manager-sub",
